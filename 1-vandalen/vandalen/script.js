5e6c1102de16fbc878a2f56cdb6a5315432fa0ff
vr222bt <vr222bt@student.lnu.se>
2014-11-07 11:47:35 +0000

    Removed some console.logs that were there for testing

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,40 +5,28 @@ var makePerson = function(persArr){
 
 	var ages = [];
 	var names = [];
-
-    ages = persArr.map(function(array){
-        return array.age;
+    ages = persArr.map(function(person){
+        return person.age;
     });
-    names = persArr.map(function(array){
-        return array.name;
+    names = persArr.map(function(person){
+        return person.name;
     });
-    console.log(ages);
-    console.log(names);
-    
     //Sums ages
     var ageSum = ages.reduce(function(ageA,ageB){
         return ageA+ageB;
     });
-    console.log("Åldersumman:" + ageSum);
-    
     //Creates object and properties
     var object = {};
     //Calculates average age
     object.averageAge = Math.round(ageSum/ages.length);
-    console.log("Medelålder:" + object.averageAge);
     //Highest age
     object.maxAge = Math.max.apply(Math,ages);
-    console.log("Högsta ålder:" + object.maxAge);
     //Lowest age
     object.minAge = Math.min.apply(Math,ages);
-    console.log("Lägsta ålder:" + object.minAge);
-    
 	function localSort(a, b) {
     return a.toString().localeCompare(b.toString());
     }
     names = names.sort(localSort);
-
-
     //Makes names into one string
     object.names = names.reduce(function(name1, name2){
         return name1 + ", " + name2;
