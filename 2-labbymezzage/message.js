a8ea8f5b2b451b0612701b4729ad4bd7eb19dba0
vr222bt <vr222bt@student.lnu.se>
2014-11-27 18:03:39 +0000

    Added methods to Message constructor

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,4 +1,23 @@
 "use strict";
 function Message(message, date){
-    
+    this.getText = function(){
+        return message;
+    };
+    this.setText = function(_text){
+        message = _text;
+    };
+    this.getDate = function(){
+        return date;
+    };
+    this.setDate = function(_date){
+        date = _date;
+    };
 }
+
+Message.prototype.toString = function(){
+    return this.getText()+" ("+this.getDate()+")";
+};
+
+Message.prototype.getHTMLText = function(){
+    return this.getText().replace("\n","<br />");
+};
