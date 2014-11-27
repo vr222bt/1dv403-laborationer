0090283c2d1a976e755d1d851ed101371121bc7d
vr222bt <vr222bt@student.lnu.se>
2014-11-27 22:58:26 +0000

    Added createMessage

--- 2-labbymezzage/run.js
+++ 2-labbymezzage/run.js
@@ -3,16 +3,25 @@
 var messageBoard = {
     
     messages: [], 
-    init:  function(e){
-        /*     
-        var message1 = new Message("test1", new Date());
-        var message2 = new Message("test2", new Date());
-        messageBoard.messages.push(message1, message2);
-        alert(messageBoard.messages[1].getText());
-        alert(message1.getText());
-        alert(message2.getText());
-        alert(message2.getDate());
-        */
+    init:  function(){
+        var textarea = document.querySelector("#text");
+        var button = document.querySelector("#button");
+    
+        button.addEventListener("click", function(e){
+        e.preventDefault();
+        messageBoard.createMessage(textarea);
+        });
+        
+    },
+    createMessage: function(textarea){
+        var messText = textarea.value;
+        //No empty messages please
+        if (messText === "") {
+            return false;
+        }
+        var mess = new Message(messText, new Date());
+        messageBoard.messages.push(mess);
+        
         
         
     }
