0688783f0a3291255917606e2da456adfd279507
vr222bt <vr222bt@student.lnu.se>
2014-11-28 00:46:54 +0000

    Added deleteMessage

--- 2-labbymezzage/run.js
+++ 2-labbymezzage/run.js
@@ -15,15 +15,59 @@ var messageBoard = {
     },
     createMessage: function(textarea){
         var messText = textarea.value;
-        //No empty messages please
+        //No empty messages please (IT BROKE?!)
         if (messText === "") {
             return false;
         }
         var mess = new Message(messText, new Date());
         messageBoard.messages.push(mess);
+        messageBoard.renderMessages();
         
         
         
+    },
+    renderMessage: function(messageID){
+        
+        var div1 = document.querySelector("#messages");
+        var div2 = document.createElement("div");
+        div2.className = "message";
+        //Deletebutton
+        var aDelete = document.createElement("a");
+        aDelete.href = "#";
+        var imgDelete = document.createElement("img");
+        imgDelete.src = "cross-button.png";
+        aDelete.appendChild(imgDelete);
+        div2.appendChild(aDelete);
+        //Message
+        var p = document.createElement("p");
+        p.innerHTML = messageBoard.messages[messageID].getHTMLText();
+        div2.appendChild(p);
+        div1.appendChild(div2);
+        //time
+        var date = document.createElement("footer");
+        date.innerHTML = messageBoard.messages[messageID].getDate();
+        div2.appendChild(date);
+
+        aDelete.onclick = function(e){
+          e.preventDefault();  
+          messageBoard.deleteMessage(messageID);
+        };
+    
+    },
+    deleteMessage: function(messageID){
+        if (confirm("Är du säker på att du vill ta bort det här meddelandet?")) {
+            messageBoard.messages.splice(messageID,1);
+            messageBoard.renderMessages();
+        }
+    },
+    renderMessages: function(){
+        //Removes all messages
+        document.querySelector("#messages").innerHTML ="";
+        
+        //Render all messages
+        for (var i = 0; i < messageBoard.messages.length; ++i) {
+            messageBoard.renderMessage(i)
+        }
     }
 };
 
