3580edcca981106bffd11db64c0884b79d588704
vr222bt <vr222bt@student.lnu.se>
2014-12-18 21:31:37 +0000

    Fixed the get and post

--- 4-thequiz/quiz.js
+++ 4-thequiz/quiz.js
@@ -4,9 +4,14 @@
 var Quiz = {
     init: function(){
         Quiz.getQuestion();
-    },
-    render: function(){
         
+
+    },
+    render: function(response){
+        var main = document.querySelector("main");
+        var question = document.createElement("p");
+        question.innerHTML = response.question;
+        main.insertBefore(question, main.firstChild);
     },
     getQuestion: function(){
         var xhr = new XMLHttpRequest();
@@ -14,8 +19,14 @@ var Quiz = {
         xhr.onreadystatechange = function (){
             if (xhr.readyState === 4) {
                 if (xhr.status == 200) {
-
                     console.log(xhr.responseText);
+                    var response = JSON.parse(xhr.responseText);
+                    console.log(response);
+                    Quiz.render(response);
+                    var button = document.querySelector("#button");
+                    button.onclick = function(){
+                        Quiz.sendAnswer(response);
+                    };
                 }
             }
             
@@ -24,6 +35,31 @@ var Quiz = {
         xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
         
         xhr.send(null);
+        
+
+    },
+    sendAnswer: function(response){
+        var xhr = new XMLHttpRequest();
+        
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4 ) {
+                if (xhr.status === 200) {
+                    console.log(xhr.responseText);
+                }
+                else
+                    console.log(xhr.responseText);
+            }
+        };
+        xhr.open("POST", response.nextURL, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        var answer = document.querySelector("#answer").value;
+        console.log(answer);
+        var rply = {
+            id: 1,
+            answer: answer,
+        };
+        xhr.send(JSON.stringify(rply));
+       
     }
     
 };
